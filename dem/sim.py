c910321545d2c8fb3eae917da076e0434a9f3538
jtimonen <juho.timonen@iki.fi>
2020-12-28 03:46:32 +0200

    create 2 sim scenarios

--- dem/sim.py
+++ dem/sim.py
@@ -3,38 +3,52 @@ import matplotlib.pyplot as plt
 from .plotting import draw_plot
 
 
-def latent_sim(idx: int = 1, N1: int = 300, N2: int = 200, sigma: float = 0.15):
-    """Simulate latent data.
+def sim(idx: int = 1, N: int = 500, sigma: float = 0.1, **sim_kwargs):
+    """Simulate data.
 
-    :return: a numpy array with shape (2, N1+N2)
-    :rtype: numpy.ndarray
+    :return: a numpy array with shape (2, N) and a numpy array with shape (N)
     """
     if idx == 1:
-        return latent_sim1(N1, N2, sigma)
+        return sim_line(N, sigma)
+    elif idx == 2:
+        return sim_spiral(N, sigma, **sim_kwargs)
     else:
-        raise ValueError("idx must be 1!")
+        raise ValueError("idx must be 1 or 2!")
 
 
-def latent_sim1(N1: int, N2: int, sigma: float):
-    """Simulate latent data."""
-    z1 = np.linspace(0, 2, N1)
-    z2 = np.sin(z1)
-    w1 = np.linspace(1, 2, N2)
-    w2 = np.sin(3 * w1) + 0.65
-    z1 = np.concatenate((z1, w1))
-    z2 = np.concatenate((z2, w2))
-
-    def normalize(x):
-        return (x - np.mean(x)) / np.std(x)
-
-    z1 = normalize(z1) + sigma * np.random.normal(size=z1.shape)
-    z2 = normalize(z2) + sigma * np.random.normal(size=z2.shape)
-    return np.vstack((z1, z2))
+def sim_line(N: int, sigma: float):
+    """A line."""
+    t = np.linspace(0, 1, N)
+    z1 = 2.0 * (t - 0.5)
+    z2 = np.zeros(z1.shape)
+    z1 = z1 + sigma * np.random.normal(size=z1.shape)
+    z2 = z2 + sigma * np.random.normal(size=z2.shape)
+    z = np.vstack((z1, z2))
+    return z, t
 
 
-def plot_latent_sim(z: np.ndarray, save_name=None, scatter_kwargs=None, **kwargs):
+def sim_spiral(N: int, sigma: float, omega: float = np.pi, decay: float = 0.0):
+    """A line."""
+    t = np.linspace(0, 1, N)
+    theta = omega * t
+    R = 1.0 * np.exp(-decay * t)
+    z1 = R * np.cos(theta)
+    z2 = R * np.sin(theta)
+    z1 = z1 + sigma * np.random.normal(size=z1.shape)
+    z2 = z2 + sigma * np.random.normal(size=z2.shape)
+    z = np.vstack((z1, z2))
+    return z, t
+
+
+def plot_sim(z: np.ndarray, t=None, save_name=None, scatter_kwargs=None, **kwargs):
     """Plot latent simulation."""
     if scatter_kwargs is None:
         scatter_kwargs = dict()
-    plt.scatter(z[0, :], z[1, :], **scatter_kwargs)
+    if t is None:
+        plt.scatter(z[0, :], z[1, :], **scatter_kwargs)
+    else:
+        plt.scatter(z[0, :], z[1, :], c=t, **scatter_kwargs)
+        plt.colorbar()
+    plt.xlim(-2, 2)
+    plt.ylim(-2, 2)
     draw_plot(save_name, **kwargs)
--- zzz.py
+++ zzz.py
@@ -1,4 +1,10 @@
-from dem.sim import latent_sim
+from dem.sim import sim, plot_sim
 
-z = latent_sim()
-print(z)
+z, t = sim(1, 300)
+plot_sim(z, t)
+
+z, t = sim(2, 300)
+plot_sim(z, t)
+
+z, t = sim(2, 500, omega=2 * 3.14, decay=0.85)
+plot_sim(z, t)

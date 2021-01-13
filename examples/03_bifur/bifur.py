98fcabe7d45a6bd49704f113f132c73886e5d805
jtimonen <juho.timonen@iki.fi>
2021-01-13 08:04:12 +0200

    edit bifur

--- examples/03_bifur/bifur.py
+++ examples/03_bifur/bifur.py
@@ -7,8 +7,8 @@ N = 1000
 z_data, t_data = sim(3, N, 0.06)
 # plot_sim(z_data, t_data)
 
-model = GenODE(2, [-0.7, -1.0], n_hidden=128)
+model = GenODE(2, [-0.7, -1.0], n_hidden=256)
 
 zz = torch.from_numpy(z_data).float()
 
-model.fit_gan(zz, n_epochs=5000, lr=0.001, plot_freq=50)
+model.fit_gan(zz, n_epochs=5000, lr=0.005, plot_freq=10)

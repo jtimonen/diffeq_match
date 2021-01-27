97405348b8dd05266d217cd5418a8dc31893f666
jtimonen <juho.timonen@iki.fi>
2021-01-27 08:57:56 +0200

    edit example 1

--- examples/01_curve/curve.py
+++ examples/01_curve/curve.py
@@ -7,8 +7,8 @@ N = 1000
 z_data, t_data = sim(1, N, sigma=0.1)
 # plot_sim(z_data, t_data)
 
-t_loc = [[-1.7, -0.15]]
-t_std = [0.05]
+t_loc = [[-1.5, 0.0]]
+t_std = [0.07]
 model = GenODE(t_loc, t_std)
 
 zz = torch.from_numpy(z_data).float()
